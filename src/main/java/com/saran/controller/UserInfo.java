1bd835470abcfc2375f9e12c44e160721fe34d60
saran <saranraj7050775@gmail.com>
2023-07-28 17:28:28 +0530

    successfully retrived user detials using jwt token

--- src/main/java/com/saran/controller/UserInfo.java
+++ src/main/java/com/saran/controller/UserInfo.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.saran.dto.ResponseDTO;
 import com.saran.entity.User;
 import com.saran.service.UserService;
 
@@ -19,21 +20,26 @@ public class UserInfo {
 	UserService userService;
 
 	@GetMapping("/my-info")
-	public ResponseEntity<User> getMyInfo(@RequestHeader("Authorization") String token){
+	public ResponseEntity<Object> getMyInfo(@RequestHeader("Authorization") String token){
+		ResponseDTO response = new ResponseDTO();
 		Claims claims = validateToken(token);
 		
 		if (claims == null) {
-			return ResponseEntity.status(401).build();			
+			response.setStatusCode("Access Denied - 401");
+			response.setMessage("Unauthorized Access");
+			return ResponseEntity.status(401).body(response);			
 		}
 		String email =  claims.getSubject();
 		User user = userService.getUserByEmail(email);
+		
+		
 		return ResponseEntity.ok(user);
 	}
 
 	private Claims validateToken(String token) {
 
 		try {
-			return Jwts.parser().setSigningKey("8Ns5L8qvBbeIcAvF6zARokHy27SZdnq1").parseClaimsJws(token).getBody();
+			return Jwts.parser().setSigningKey("SECRET_KEY").parseClaimsJws(token).getBody();
 		} catch (Exception e) {
 			return null;
 		}
--- src/main/java/com/saran/service/JwtService.java
+++ src/main/java/com/saran/service/JwtService.java
@@ -23,7 +23,7 @@ public class JwtService {
 	private String createToken(Map<String, Object> claims, String email) {
 		return Jwts.builder().setClaims(claims).setSubject(email).setIssuedAt(new Date(System.currentTimeMillis()))
 //						.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
-						.signWith(SignatureAlgorithm.HS256, SECRET).compact();
+						.signWith(SignatureAlgorithm.HS512, SECRET).compact();
 	}
 
 }
